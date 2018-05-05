c76a98001807e12191a75829aa6eadcf29d5c586
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 23:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ function getCommits(el) {
 }
 
 function getBranches(el) {
-  const name = el.dataset.repo;
-  const username = el.dataset.user;
+  const name = el.dataset.repository;
+  const username = el.dataset.username;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayBranches);
   req.open("GET", 'https://api.github.com/repos/' + username + '/' + name + '/branches');

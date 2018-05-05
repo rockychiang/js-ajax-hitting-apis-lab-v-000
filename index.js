0ed35a900fa10ef3502782fa3efe925a542861cd
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 22:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function getRepositories() {
 
 function getCommits(el) {
   const name = el.dataset.repo.name;
-  const username = el.dataset.repo.owner.login;
+  const username = el.dataset.repo.author.login;
   const req = new XMLHttpRequest();
   req.addEventListener("load", displayCommits);
   req.open("GET", 'https://api.github.com/repos/' + username + '/' + name + '/commits');

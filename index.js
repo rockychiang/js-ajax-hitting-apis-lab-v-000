220a02b695a675ad7e3535cb2b342e700ee891bd
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-05 22:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function getRepositories() {
 
 function getCommits(el) {
   const name = el.dataset.repo.name;
-  console.log(el.dataset.repo);
+  console.log(el.dataset.repo.owner);
   const username = el.dataset.repo.author.login;
   const req = new XMLHttpRequest();
   req.addEventListener("load", displayCommits);
